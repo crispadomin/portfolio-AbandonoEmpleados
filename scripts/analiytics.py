8eba2a41468e868d694ea42ebf913817f151e0ab
crisp <crispadomin@gmail.com>
2025-02-03 13:11:52 +0100

    Estudio variables categóricas

--- scripts/analiytics.py
+++ scripts/analiytics.py
@@ -11,3 +11,24 @@ df.isna().sum().sort_values(ascending = False)
 
 df.drop(columns = ['anos_en_puesto','conciliacion'], inplace = True)
 print(df)
+
+    ##----- EDA Variables categóricas -----##
+def graficos_eda_categoricos(cat):
+    
+        # Calculamos el número de filas que necesitamos para la representación
+    from math import ceil
+    filas = ceil(cat.shape[1] / 2)
+
+        # Definimos el gráfico
+    f, ax = plt.subplots(nrows = filas, ncols = 2, figsize = (16, filas * 6))
+
+        # Aplanamos para iterar por el gráfico como si fuera de 1 dimensión en lugar de 2
+    ax = ax.flat 
+
+        # Creamos el bucle que va añadiendo gráficos
+    for cada, variable in enumerate(cat):
+        cat[variable].value_counts().plot.barh(ax = ax[cada])
+        ax[cada].set_title(variable, fontsize = 12, fontweight = "bold")
+        ax[cada].tick_params(labelsize = 12)
+
+graficos_eda_categoricos(df.select_dtypes('O'))
